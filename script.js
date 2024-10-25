7852de799fed96a83036c57d9fbabc3a76eebec3
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-10-25 01:38:25 -0300

    Finaliza lógica que altera os dados dos usuários.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('main-form');
 const btnListOS = document.getElementById('btnListOS');
 const displayOS = document.getElementById('display-OS');
+const updateOS = document.querySelector('.update-users-os');
 
 // Armazena as ordens de serviço
 let usersOS = [];
@@ -20,23 +21,40 @@ form.addEventListener('submit', (e) => {
 
     addUsersOs(name, sector, email, problem);
     form.reset();
-    console.log(usersOS);
 })
 
 function displayNewOs() {
     displayOS.innerHTML = '';
+    const tableHeader = `
+        <tr class="has-header">
+            <th>Nome</th>
+            <th>Setor</th>
+            <th>E-mail</th>
+            <th>Problema</th>
+            <th>Excluir OS</th>
+            <th>Atualizar OS</th>
+        </tr>
+    `;
+
     usersOS.forEach((user, index) => {
         const userOs = `
-            <div class="listOS-item">
-                <h4>${user.name}</h4>
-                <p>${user.sector}</p>
-                <p>${user.email}</p>
-                <p>${user.problem}</p>
-                <button onclick="removeUserOs(${index})">Excluir OS</button>
-            </div>
+            <tr class="listOS-item">
+                <td>${user.name}</td>
+                <td>${user.sector}</td>
+                <td>${user.email}</td>
+                <td>${user.problem}</td>
+                <td><button onclick="removeUserOs(${index})">Excluir</button></td>
+                <td><button onclick="updateUserOs(${index})">Atualizar</button></td>
+            </tr>
         `
         displayOS.insertAdjacentHTML('beforeend', userOs);
+
+        if(!document.querySelector('.has-header')){
+            displayOS.querySelector('.listOS-item').insertAdjacentHTML('beforebegin', tableHeader);
+        }
     })
+
+    usersOS.length > 0 ? displayOS.classList.remove('hidden') : displayOS.classList.add('hidden');
 }
 
 // Exibe as OS abertas quando clicado no botão listar
@@ -48,4 +66,31 @@ btnListOS.addEventListener('click', () => {
 function removeUserOs(index) {
     usersOS.splice(index, 1);
     displayNewOs();
+}
+
+// Atualiza os dados
+function updateUserOs(index) {
+    updateOS.classList.toggle('hidden');
+    usersOS.length === 0 ? updateOS.classList.add('hidden') : null;
+
+    updateOS.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const updateName = document.getElementById('updateName').value;
+        const updateSector = document.getElementById('updateSector').value;
+        const updateEmail = document.getElementById('updateEmail').value;
+        const updateProblem = document.getElementById('updateProblem').value;
+
+        usersOS.forEach((user, i) => {
+            if(i == index){
+                updateName != '' ? user.name = updateName : null;
+                updateSector != '' ? user.sector = updateSector : null;
+                updateEmail != '' ? user.email = updateEmail : null;
+                updateProblem != '' ? user.problem = updateProblem : null;
+
+                displayNewOs();
+                updateOS.classList.toggle('hidden');
+            }
+        })
+        // displayOS.classList.toggle('hidden');
+    })
 }
